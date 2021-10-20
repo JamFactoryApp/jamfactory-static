d3fa4165e930a5cbe08e274e8d6c6ec9d1afcc61
Tim <xmrtimex@gmail.com>
2021-10-20 14:02:10 +0200

    :art: Remove unused stuff

--- webpack.config.js
+++ webpack.config.js
@@ -51,8 +51,6 @@ module.exports = {
     },
     optimization: {
         minimizer: [
-            // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
-            // `...`,
             new CssMinimizerPlugin(),
         ],
     },
