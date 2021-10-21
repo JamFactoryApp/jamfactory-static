e659714ec0082c3ac6e3eef9f1c6994c91806bd0
Tim <xmrtimex@gmail.com>
2021-10-21 22:48:37 +0200

    :mag: Add SEO content

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const CopyPlugin = require('copy-webpack-plugin');
 const fs = require("fs");
 
 let htmlPageNames = [];
@@ -15,7 +16,6 @@ let multipleHtmlPlugins = htmlPageNames.map(name => {
     return new HtmlWebpackPlugin({
         template: `./src/${name}.html`,
         filename: `${name}.html`,
-        title: "JamFactory",
         favicon: "./src/assets/favicon.ico",
         chunks: ['main']
     })
@@ -29,6 +29,10 @@ module.exports = {
     },
     plugins: [
         new MiniCssExtractPlugin(),
+        new CopyPlugin({
+            patterns: [
+            { from: 'src/assets/robots.txt', to: 'robots.txt' }
+        ]})
     ].concat(multipleHtmlPlugins),
     module: {
         rules: [
